24b815d47419194a38b202de66f9c23234615b2a
oneku16 <eku.ulanov@yandex.com>
2023-07-06 17:10:29 +0600

    created modules

--- configurations/settings.py
+++ configurations/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'djoser',
+
+    'users',
+    'hotels',
+
 ]
 
 MIDDLEWARE = [
